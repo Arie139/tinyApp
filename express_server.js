fc40fa625b1cc8e3ce8d49bc46f8c29201787345
Ariel <see-har@hotmail.com>
2021-11-10 19:36:43 +0000

    update

---
+++ express_server.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const app = express();
+const PORT = 8080;
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+app.get("/", (req, res) => {
+  res.send("Hello!"); // homepage
+});
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+app.post("/urls", (req, res) => {
+  let shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect(`/urls/${shortURL}`);
+}); // new short url redirect to short url
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars); // new url
+});
+app.post("/urls/:shortURL", (req, res) => {
+  const shortURL = req.params.shortURL;
+  const longURL = req.body.longURL;
+  urlDatabase[shortURL] = longURL;
+  res.redirect('/urls'); //update url in database
+});
+app.post("/urls/:shortURL/delete", (req, res) => {
+  const shortURL = req.params.shortURL;
+  delete urlDatabase[shortURL];
+  res.redirect('/urls');
+}); // delete short url
+app.get("/u/:shortURL", (req, res) => {
+  shortURL = req.params.shortURL;
+  const longURL = urlDatabase[shortURL];
+  res.redirect(longURL); // short url redirect to long
+});
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
+
+function generateRandomString() {
+  let r = (Math.random() + 1).toString(36).substring(6)
+  return r;
+}// rnd key generator
