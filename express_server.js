8c2387f3c3d16d09c51fbf6a7fd5d201eafeca89
Ariel <see-har@hotmail.com>
2021-11-13 22:22:11 +0000

    encrypt pass

--- express_server.js
+++ express_server.js
@@ -3,6 +3,7 @@ const app = express();
 const PORT = 8080;
 const bodyParser = require("body-parser");
 const cookieParser = require('cookie-parser')
+const bcrypt = require('bcryptjs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
 
@@ -20,13 +21,19 @@ const users = {
     password: "dishwasher-funk"
   } //user database
 };
+console.log(users);
+
 function generateRandomString() {
   let r = (Math.random() + 1).toString(36).substring(6)
   return r;
 }// rnd key generator 6 alphanumeric
 
 const getEmail = user_id => {
-  return (users[user_id]) ? users[user_id].email : null;
+  if(users[user_id]) {
+    return users[user_id].email;
+  } else {
+    return null;
+  }
 };//email from id
 
 const getUserByEmail = email => {
@@ -37,15 +44,6 @@ const getUserByEmail = email => {
   }
 };  //find email in users
  
-
-//  const findPass = (userId, password) => {
-//   if (users[userId].password === password) {
-//     return true;
-//   }
-//   return false;
-// };// find password match in users
-
-
 const urlDatabase = {
   b6UTxQ: {
     longURL: "https://www.tsn.ca",
@@ -66,7 +64,6 @@ const getUrls = user => {
   }
   return urls;
 };
-console.log(getUrls('95xkr9'));
 
 app.get("/", (req, res) => {
   if((req.cookies['user_id'])) {
@@ -108,14 +105,18 @@ app.get("/urls/new", (req, res) => {
 app.post("/urls", (req, res) => {
   let shortURL = generateRandomString();
   urlDatabase[shortURL] = {
-    longUrl:req.body.longURL,
+    longURL: req.body.longURL,
     userID: req.cookies['user_id']
   }
+  console.log(urlDatabase);
   return res.redirect(`/urls/${shortURL}`);
 }); // new short url redirect to short url
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { 
+  if (!req.cookies['user_id']) {
+    return res.redirect('/login');
+  }
+    const templateVars = { 
     shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL].longURL,
     user_id: req.cookies["user_id"], 
@@ -126,7 +127,7 @@ app.get("/urls/:shortURL", (req, res) => {
 
 app.post("/urls/:shortURL", (req, res) => {
   if (!req.cookies['user_id']) {
-    return res.render('error_access')
+    return res.redirect('/login');
   } else if (req.cookies['user_id'] === urlDatabase[req.params.shortURL].userID) {
     const shortURL = req.params.shortURL;
     const longURL = req.body.longURL;
@@ -138,7 +139,7 @@ app.post("/urls/:shortURL", (req, res) => {
 app.post("/urls/:shortURL/delete", (req, res) => {
   const shortURL = req.params.shortURL;
   if (!req.cookies['user_id'] || req.cookies['user_id'] !== urlDatabase[shortURL].userID) {
-    return res.render('error_access')
+    return res.redirect('/login');
   } else {
     delete urlDatabase[shortURL];
     return res.redirect('/urls');
@@ -169,12 +170,15 @@ app.post('/login', (req, res) => {
     return res.status(400).send('Please enter a valid password');
   }
   let user = getUserByEmail(req.body.email);
-  if(!user || user.password !== req.body.password) {
-    return res.status(400).send('User or password incorrect.');
-  } else if (user.password === req.body.password) {
-    res.cookie('user_id', user.id);
-    return res.redirect('/urls');
+  if(!user) {
+    return res.status(400).send('User incorrect');
   }
+  console.log(user);
+  if (!bcrypt.compareSync(req.body.password, users[user.user_id].password)) {
+    return res.status(400).send('Password incorrect');
+  }
+  res.cookie('user_id', user.user_id);
+  return res.redirect('/urls');
 })//login page user input
 
 app.post('/logout', (req, res) => {
@@ -187,7 +191,7 @@ app.get("/register", (req, res) => {
     user_id: req.cookies["user_id"], 
     email: getEmail(req.cookies['user_id'])
   };
-  res.render('register', templateVars);
+  return res.render('register', templateVars);
 });// register page
 
 app.post('/register', (req, res) => {
@@ -199,13 +203,15 @@ app.post('/register', (req, res) => {
   let userId = getUserByEmail(req.body.email);
   if (!userId) {
     genId = generateRandomString();
-    users[genId] = { user_id: genId, email: req.body.email, password: req.body.password }
+    const hashedPassword = bcrypt.hashSync(req.body.password, 10);
+    users[genId] = { user_id: genId, email: req.body.email, password: hashedPassword};
+    console.log(users);
     res.cookie("user_id", genId);
+    return res.redirect('/urls');
   } else if (userId) {
     return res.status(403).send('There\'s already an account associated to this email.')
   }
-  return res.redirect('/urls');
-})
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
